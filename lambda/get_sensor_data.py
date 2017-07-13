09e6aca45782c50ef931352099b0f6d3d34c8773
Nathan Glover <nathan@glovers.id.au>
2017-07-14 01:47:52 +0800

    Updated schema

--- lambda/get_sensor_data.py
+++ lambda/get_sensor_data.py
@@ -2,6 +2,7 @@ import boto3
 import urllib.request
 import json
 
+
 def build_response(message):
     return {
         "dialogAction": {
@@ -13,6 +14,7 @@ def build_response(message):
             }
         }
     }
+
 
 def lambda_handler(event, context):
     if 'GetSensorData' == event['currentIntent']['name']:
@@ -26,6 +28,16 @@ def lambda_handler(event, context):
         msg = ""
         for item in json_data['data']:
             if item['sensor_id'] == ("NODE-" + sensor_id):
-                msg += "*Node ID*: " + item['sensor_id'] + "\n*Node MAC*: " + item['sensor_mac'] + "\n*Latitude*: " + str(item['location_lat']) + "\n*Longitude*: " + str(item['location_lon']) + "\n*Last Updated*: " + item['timestamp']
+                msg += "*Node ID*: " + item['sensor_id'] + "\n*Node MAC*: " + item[
+                    'sensor_mac'] + "\n*Latitude*: " + str(item['location_lat']) + "\n*Longitude*: " + str(
+                    item['location_lon']) + "\n*Last Updated*: " + item['timestamp'] + "\n*GPS Datestamp*: " + item[
+                           'datestamp'] + "\n*Altitude*: " + str(item['altitude']) + "\n*Velocity*: " + str(item[
+                                                                                                                'velocity']) + "\n*GPS Error*: " + str(
+                    item['GPSerror']) + "\n*IMU Error*: " + str(item[
+                                                                    'IMUerror']) + "\n*Valid Orientation*: " + str(
+                    item['rightdirection']) + "\n*Course*: " + str(item[
+                                                                       'course']) + "\n*Satellites*: " + str(
+                    item['nsats']) + "\n*SNR1*: " + str(item['snr1']) + "\n*SNR2*: " + \
+                       str(item['snr2']) + "\n*SNR3*: " + str(item['snr3']) + "\n*SNR4*: " + str(item['snr4'])
 
         return build_response(msg)
